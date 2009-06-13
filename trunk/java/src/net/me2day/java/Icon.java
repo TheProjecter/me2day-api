16bbd23eb84572b982e6f6b8465e3872828dc3a3
xrathx <xrathx@1bf124b7-1333-0410-8393-938a6bc557f7>
2009-06-13 12:15:18 +0000

    file attachment feature was added. thanks to 꼬룸!

---
+++ trunk/java/src/net/me2day/java/Icon.java
@@ -0,0 +1,57 @@
+package net.me2day.java;
+
+import java.io.Serializable;
+import java.net.URL;
+
+public class Icon implements Serializable {
+	private static final long serialVersionUID = -767861645667681261L;
+	
+	private int index;
+	private int type;
+	private URL url;
+	private boolean isDefault;
+	
+	public Icon() {
+		
+	}
+	
+	public void setIndex( int index ) 
+	{
+		this.index = index;
+	}
+	
+	public int getIndex() 
+	{
+		return index;
+	}
+	
+	public void setType( int type ) 
+	{
+		this.type = type;
+	}
+	
+	public int getType() 
+	{
+		return type;
+	}
+	
+	public void setURL( URL url ) 
+	{
+		this.url = url;
+	}
+	
+	public URL getURL() 
+	{
+		return url;
+	}
+	
+	public void setDefault( boolean value ) 
+	{
+		this.isDefault = value;
+	}
+	
+	public boolean isDefault() 
+	{
+		return isDefault;
+	}
+}
