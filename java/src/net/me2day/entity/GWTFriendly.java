532144f3427ec04c04c54f7c8b3a0d2d65f04aff
xrathx <xrathx@1bf124b7-1333-0410-8393-938a6bc557f7>
2009-09-08 04:02:11 +0000

    Added a comment.

--- java/src/net/me2day/entity/GWTFriendly.java
+++ java/src/net/me2day/entity/GWTFriendly.java
@@ -1,5 +1,13 @@
 package net.me2day.entity;
 
+/**
+ *
+ * @author Jang-Ho Hwang, rath@xrath.com
+ * @version 1.0
+ */
 public interface GWTFriendly {
+    /**
+     * Create a new object that fully transportable within GWT environment.
+     */
 	public Object toGWT();
 }
